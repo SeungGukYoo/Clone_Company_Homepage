981615486b617a9b8b5733532968e2401bdcd535
SeungGukYoo <dbtmd878@gmail.com>
2022-12-08 22:23:06 +0900

    feat:슬라이더 로직 추가 및 Swiper 플러그인 제거, CSS 수정

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
-// header
+const lists = document.querySelectorAll(".popUp ul li");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+const ul = document.querySelector("#figure .inner ul");
+
+ul.prepend(ul.lastElementChild);
+ul.style.marginLeft = "-100%";
+prevBtn.addEventListener("click", () => {
+  new Anim(ul, {
+    prop: "margin-left",
+    value: "0%",
+    duration: 1000,
+    callback: (e) => {
+      ul.style.marginLeft = "-100%";
+      ul.prepend(ul.lastElementChild);
+      const li = ul.querySelectorAll("li")[1];
+      activeSmallBanner(li);
+    }
+  });
+});
+
+nextBtn.addEventListener("click", () => {
+  new Anim(ul, {
+    prop: "margin-left",
+    value: "-200%",
+    duration: 1000,
+    callback: () => {
+      ul.style.marginLeft = "-100%";
+      ul.append(ul.firstElementChild);
+      const li = ul.querySelectorAll("li")[1];
+      activeSmallBanner(li);
+    }
+  });
+});
+
+setInterval(() => {
+  new Anim(ul, {
+    prop: "margin-left",
+    value: "-200%",
+    duration: 1500,
+    callback: () => {
+      ul.style.marginLeft = "-100%";
+      ul.append(ul.firstElementChild);
+      const li = ul.querySelectorAll("li")[1];
+      activeSmallBanner(li);
+    }
+  });
+}, 7000);
+
 (function btnAddActive() {
   const menuBtn = document.querySelector(".menuBtn");
   const sideMenu = document.querySelector(".sideMenu");
@@ -9,17 +57,8 @@
   });
 })();
 
-// figure
-var swiper = new Swiper(".swiperContainer", {
-  loop: true,
-  loopAdditionalSlides: 1,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: true
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
+function activeSmallBanner(list) {
+  const index = list.dataset.index;
+  lists.forEach((list) => list.classList.remove("on"));
+  lists[index - 1].classList.add("on");
+}
