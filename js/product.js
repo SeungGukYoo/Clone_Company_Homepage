929cca458e53323f3d704ec17d0bac47b99247ca
SeungGukYoo <dbtmd878@gmail.com>
2022-12-22 22:10:15 +0900

    feat: 유튜브 추가 및 반응형 SCSS

--- js/product.js
+++ js/product.js
@@ -1,6 +1,19 @@
 const product_url =
-  'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=HDPEPIPE&key=AIzaSyCzFsvPUv4oQBwHeCsYH1FxMueWv-GAiY8';
+  'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=HDPEPIPE&key=AIzaSyCzFsvPUv4oQBwHeCsYH1FxMueWv-GAiY8';
+const video = document.querySelector('.video');
 
 fetch(product_url)
   .then((value) => value.json())
-  .then((data) => console.log(data));
+  .then((data) => data.items[0])
+  .then((item) => {
+    video.innerHTML = `
+  <iframe 
+    id="existing-iframe-example" 
+    width="640"
+    height="360"
+		src="https://www.youtube.com/embed/${item.id.videoId}?enablejsapi=1" 
+    frameborder="0"
+		style="border: solid 4px #37474F">
+  </iframe>
+`;
+  });
